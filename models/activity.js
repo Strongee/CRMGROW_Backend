802f5335dfd0cfb79ef66c03a6c5f94922b57f67
superwebtop <superwebtop@outlook.com>
2019-04-21 02:34:22 -0700

    model tracking fix

--- models/activity.js
+++ models/activity.js
@@ -8,6 +8,8 @@ const Activity = mongoose.model('activity',{
     follow_ups:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'follow_up' }],
     notes:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'note' }],
     phone_logs:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'phone_log' }],
+    videos:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'video' }],
+    pdf: [{ type: mongoose.Schema.Types.ObjectId, ref: 'pdf' }],
     video_trackers:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'video_tracker' }],
     email: [{ type: mongoose.Schema.Types.ObjectId, ref: 'email' }],
     contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'contact' }],
