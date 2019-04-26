717563bd857612d7153e206ef7e071c1293d638f
superwebtop <superwebtop@outlook.com>
2019-04-26 06:24:23 -0700

    staging config

--- constants/urls.js
+++ constants/urls.js
@@ -1,13 +1,13 @@
   if(process.env.NODE_ENV === 'production') {
     urls = {
-      DOMAIN_URL: 'https://teamgrow.co/',
-      API_URL: 'https://teamgrow.co/api',
-      EMAIL_AUTHORIZE_URL: 'https://teamgrow.co/api/user/authorize-outlook',
-      VIDEO_THUMBNAIL_URL:  'https://teamgrow.co/api/video/thumbnail/',
-      PDF_PREVIEW_URL: 'https://teamgrow.co/api/pdf/preview/',
-      FILE_URL: 'https://teamgrow.co/api/file/',
-      MATERIAL_VIEW_VIDEO_URL: 'https://teamgrow.co/material/view/video/',
-      MATERIAL_VIEW_PDF_URL: 'https://teamgrow.co/material/view/pdf/'
+      DOMAIN_URL: 'http://18.220.168.230/',
+      API_URL: 'http://18.220.168.230/api',
+      EMAIL_AUTHORIZE_URL: 'http://18.220.168.230/api/user/authorize-outlook',
+      VIDEO_THUMBNAIL_URL:  'http://18.220.168.230/api/video/thumbnail/',
+      PDF_PREVIEW_URL: 'http://18.220.168.230/api/pdf/preview/',
+      FILE_URL: 'http://18.220.168.230/api/file/',
+      MATERIAL_VIEW_VIDEO_URL: 'http://18.220.168.230/material/view/video/',
+      MATERIAL_VIEW_PDF_URL: 'http://18.220.168.230/material/view/pdf/'
     }
   } else {
     urls = {
