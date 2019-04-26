be2d0200af7295f4689a8397767f1dd69a2695ae
superwebtop <superwebtop@outlook.com>
2019-04-26 10:51:42 -0700

    audio

--- routes/admin/pdf.js
+++ routes/admin/pdf.js
@@ -9,29 +9,28 @@ const uuidv1 = require('uuid/v1')
 const PDFCtrl = require('../../controllers/admin/pdf')
 const UserCtrl = require('../../controllers/admin/user')
 const { catchError } = require('../../controllers/error')
+const config  = require('../config/config')
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+  accessKeyId: config.AWS.AWS_ACCESS_KEY,
+  secretAccessKey: config.AWS.AWS_SECRET_ACCESS_KEY,
+  region: config.AWS.AWS_S3_REGION
 })
 
 const router = express.Router()
 
 const storage = multerS3({
     s3: s3,
-    bucket: process.env.BUCKET_NAME,
+    bucket: config.AWS.AWS_S3_BUCKET_NAME,
     acl: 'public-read',
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
     },
     key: function (req, file, cb) {
-      const today = new Date()
-      const year = today.getYear()
-      const month = today.getMonth()
-      cb(null, 'pdf' + year + '/' + month + '/' + uuidv1() + '.' + mime.extension(file.mimetype))
+      cb(null, 'video' + year + '/' + month + '/' + uuidv1() + '.' + mime.extension(file.mimetype))
     },
   })
+
 
 
 const upload = multer({
