3c69ecb2ceb76ce3576573a588710fd111754208
superwebtop <superwebtop@outlook.com>
2019-06-04 18:28:44 -0700

    weekly daily report fix

--- controllers/user.js
+++ controllers/user.js
@@ -28,8 +28,125 @@ const accountSid = config.TWILIO.TWILIO_SID
 const authToken = config.TWILIO.TWILIO_AUTH_TOKEN
 const client = require('twilio')(accountSid, authToken)
 
-let daily_report
-let weekly_report
+const daily_report = new CronJob({
+  // Run at 21:00 Central time, only on friday
+  cronTime: '00 21 * * 1-6',
+  onTick: async function() {
+    const users = User.find({daily_report: true})
+    sgMail.setApiKey(config.SENDGRID.SENDGRID_KEY);
+  
+    const start = new Date();
+    start.setHours(0,0,0,0);
+
+    const end = new Date();
+    end.setHours(20,59,59,999);
+    for( let i = 0; i< users.length; i ++){
+      const activity = await Activity.find({user :currentUser.id, created_at: {$gte: start, $lt: end}})
+      const currentUser = users[i]
+
+      let now = moment();
+      const today = now.format("dddd, MMMM Do YYYY")
+      
+      let contacts = []
+      for (let j =0; j < activity.length; j ++){
+        const contact = await Contact.findOne({_id: activity[j].contacts})
+        let content = "<div class='content' style='display:flex; padding-top:20px; margin-right:10px;max-width: 500px;justify-content:space-around;padding-left:30px; border-bottom: 1px solid #afaaaa;'><div class='avatar' style='margin-right:20px;'><img style='margin:auto;' src='" + urls.AVATAR_URL+"' width='60px' height='60px' /></div>" + 
+        "<div class='contact'><h3>" + contact.first_name + "</h3><p style='margin: 0px'>" + contact.email +" " + contact.cell_phone + "</p>" +
+        "<p style='margin: 0px'>" + activity[j].content +"</p></div>" + 
+        "<button style='background-color: white; color:#0078d4; max-width:100px; height:30px; margin: auto 10px; border: 1px solid; border-left: 4px solid #0078d4; cursor:pointer;'><a href='" + urls.CONTACT_PAGE_URL + contact.id + "'>View Contact</a></button></div>"
+        contacts.push(content)
+      }
+    
+      const _follow_up = await FollowUp.find({user :currentUser.id, status: 0, due_date: {$lt: current_time}});
+      let overdue = [];
+      
+      for(let j = 0; j < _follow_up.length; j ++){
+        const contact = await Contact.findOne({_id: _follow_up[j].contact}) 
+        const _overdue = "<div class='content' style='display:flex; padding-top:20px; margin-right:10px;max-width: 500px;justify-content:space-around;padding-left:30px; border-bottom: 1px solid #afaaaa;'><div class='avatar' style='margin-right:20px;'><img style='margin:auto;' src='" + urls.AVATAR_URL+"' width='60px' height='60px' /></div>" + 
+        "<div class='contact'><h3>" + contact.first_name + "</h3><p style='margin: 0px'>" + contact.email +" " + contact.cell_phone + "</p>" +
+        "<p style='margin: 0px'>" + _follow_up[j].content +"</p></div>" + 
+        "<button style='background-color: white; color:#0078d4; max-width:100px; height:30px; margin: auto 10px; border: 1px solid; border-left: 4px solid #0078d4; cursor:pointer;'><a href='" + urls.FOLLOWUP_PAGE_URL + contact.id + "'>View FollowUp</a></button></div>"
+        overdue.push(_overdue)
+      }
+    
+      const msg = {
+        to: currentUser.email,
+        from: mail_contents.DAILY_REPORT.MAIL,
+        subject: mail_contents.DAILY_REPORT.SUBJECT,
+        templateId: config.SENDGRID.SENDGRID_DAILY_REPORT_TEMPLATE,
+        dynamic_template_data: {
+          contacts: contacts,
+          overdue: overdue,
+          day: today
+        },
+      }
+      await sgMail.send(msg)
+    } 
+  },
+  start: true,
+  timeZone: 'US/Central'
+});
+
+const weekly_report = new CronJob({
+  // Run at 21:00 Central time, only on friday
+  cronTime: '00 21 * * Sun',
+  onTick: async function() {
+    const users = User.find({weekly_report: true})
+    sgMail.setApiKey(config.SENDGRID.SENDGRID_KEY);
+  
+    today = new Date();
+    let day = today.getDay(),
+        diff = today.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+    const monday = new Date(today.setDate(diff));
+    monday.setHours(0,0,0,0);
+
+    const end = new Date();
+    end.setHours(20,59,59,999);
+    for( let i = 0; i< users.length; i ++){
+      const currentUser = users[i]
+      const activity = await Activity.find({user :currentUser.id, created_at: {$gte: monday, $lt: end}}).sort({_id : -1 }).limit(15)
+      let now = moment();
+      const today = now.format("dddd, MMMM Do YYYY")
+      
+      let contacts = []
+      for (let j =0; j < activity.length; j ++){
+        const contact = await Contact.findOne({_id: activity[j].contacts})
+        let content = "<div class='content' style='display:flex; padding-top:20px; margin-right:10px;max-width: 500px;justify-content:space-around;padding-left:30px; border-bottom: 1px solid #afaaaa;'><div class='avatar' style='margin-right:20px;'><img style='margin:auto;' src='" + urls.AVATAR_URL+"' width='60px' height='60px' /></div>" + 
+        "<div class='contact'><h3>" + contact.first_name + "</h3><p style='margin: 0px'>" + contact.email +" " + contact.cell_phone + "</p>" +
+        "<p style='margin: 0px'>" + activity[j].content +"</p></div>" + 
+        "<button style='background-color: white; color:#0078d4; max-width:100px; height:30px; margin: auto 10px; border: 1px solid; border-left: 4px solid #0078d4; cursor:pointer;'><a href='" + urls.CONTACT_PAGE_URL + contact.id + "'>View</a></button></div>"
+        contacts.push(content)
+      }
+    
+      const _follow_up = await FollowUp.find({user :currentUser.id, status: 0, due_date: {$lt: current_time}});
+      let overdue = [];
+      
+      for(let j = 0; j < _follow_up.length; j ++){
+        const contact = await Contact.findOne({_id: _follow_up[j].contact}) 
+        const _overdue = "<div class='content' style='display:flex; padding-top:20px; margin-right:10px;max-width: 500px;justify-content:space-around;padding-left:30px; border-bottom: 1px solid #afaaaa;'><div class='avatar' style='margin-right:20px;'><img style='margin:auto;' src='" + urls.AVATAR_URL+"' width='60px' height='60px' /></div>" + 
+        "<div class='contact'><h3>" + contact.first_name + "</h3><p style='margin: 0px'>" + contact.email +" " + contact.cell_phone + "</p>" +
+        "<p style='margin: 0px'>" + _follow_up[j].content +"</p></div>" + 
+        "<button style='background-color: white; color:#0078d4; max-width:100px; height:30px; margin: auto 10px; border: 1px solid; border-left: 4px solid #0078d4; cursor:pointer;'><a href='" + urls.FOLLOWUP_PAGE_URL + contact.id + "'>View FollowUp</a></button></div>"
+        overdue.push(_overdue)
+      }
+
+      const msg = {
+        to: currentUser.email,
+        from: mail_contents.DAILY_REPORT.MAIL,
+        subject: mail_contents.DAILY_REPORT.SUBJECT,
+        templateId: config.SENDGRID.SENDGRID_DAILY_REPORT_TEMPLATE,
+        dynamic_template_data: {
+          contacts: contacts,
+          overdue: overdue,
+          day: today
+        },
+      }
+      await sgMail.send(msg)
+    }
+  },
+  start: true,
+  timeZone: 'US/Central'
+});
 
 const signUp = async (req, res) => {
     const errors = validationResult(req)
@@ -713,63 +830,6 @@ const dailyReport = async(req, res) => {
   user['daily_report'] = true;
 
   await user.save()
-
-  daily_report = new CronJob({
-    // Run at 21:00 Central time, only on friday
-    cronTime: '00 21 * * 1-6',
-    onTick: async function() {
-      const { currentUser } = req
-      sgMail.setApiKey(config.SENDGRID.SENDGRID_KEY);
-    
-      const start = new Date();
-      start.setHours(0,0,0,0);
-  
-      const end = new Date();
-      end.setHours(20,59,59,999);
-      const activity = await Activity.find({user :currentUser.id, created_at: {$gte: start, $lt: end}})
-
-      let now = moment();
-      const today = now.format("dddd, MMMM Do YYYY")
-      
-      let contacts = []
-      for (let i =0; i < activity.length; i ++){
-        const contact = await Contact.findOne({_id: activity[i].contacts})
-        let content = "<div class='content' style='display:flex; padding-top:20px; margin-right:10px;max-width: 500px;justify-content:space-around;padding-left:30px; border-bottom: 1px solid #afaaaa;'><div class='avatar' style='margin-right:20px;'><img style='margin:auto;' src='" + urls.AVATAR_URL+"' width='60px' height='60px' /></div>" + 
-        "<div class='contact'><h3>" + contact.first_name + "</h3><p style='margin: 0px'>" + contact.email +" " + contact.cell_phone + "</p>" +
-        "<p style='margin: 0px'>" + activity[i].content +"</p></div>" + 
-        "<button style='background-color: white; color:#0078d4; max-width:100px; height:30px; margin: auto 10px; border: 1px solid; border-left: 4px solid #0078d4; cursor:pointer;'><a href='" + urls.CONTACT_PAGE_URL + contact.id + "'>View Contact</a></button></div>"
-        contacts.push(content)
-      }
-    
-      const _follow_up = await FollowUp.find({user :currentUser.id, status: 0, due_date: {$lt: current_time}});
-      let overdue = [];
-      
-      for(let i = 0; i < _follow_up.length; i ++){
-        const contact = await Contact.findOne({_id: _follow_up[i].contact}) 
-        const _overdue = "<div class='content' style='display:flex; padding-top:20px; margin-right:10px;max-width: 500px;justify-content:space-around;padding-left:30px; border-bottom: 1px solid #afaaaa;'><div class='avatar' style='margin-right:20px;'><img style='margin:auto;' src='" + urls.AVATAR_URL+"' width='60px' height='60px' /></div>" + 
-        "<div class='contact'><h3>" + contact.first_name + "</h3><p style='margin: 0px'>" + contact.email +" " + contact.cell_phone + "</p>" +
-        "<p style='margin: 0px'>" + _follow_up[i].content +"</p></div>" + 
-        "<button style='background-color: white; color:#0078d4; max-width:100px; height:30px; margin: auto 10px; border: 1px solid; border-left: 4px solid #0078d4; cursor:pointer;'><a href='" + urls.FOLLOWUP_PAGE_URL + contact.id + "'>View FollowUp</a></button></div>"
-        overdue.push(_overdue)
-      }
-    
-      const msg = {
-        to: currentUser.email,
-        from: mail_contents.DAILY_REPORT.MAIL,
-        subject: mail_contents.DAILY_REPORT.SUBJECT,
-        templateId: config.SENDGRID.SENDGRID_DAILY_REPORT_TEMPLATE,
-        dynamic_template_data: {
-          contacts: contacts,
-          overdue: overdue,
-          day: today
-        },
-      }
-      await sgMail.send(msg)
-    },
-    start: true,
-    timeZone: 'US/Central'
-  });
-  
   return res.send({
     status: true
   })
@@ -780,72 +840,10 @@ const weeklyReport = async(req, res) => {
   const user = req.currentUser
 
   user['weekly_report'] = true;
-
   await user.save()
 
-  weekly_report = new CronJob({
-    // Run at 21:00 Central time, only on friday
-    cronTime: '00 21 * * Sun',
-    onTick: async function() {
-      const { currentUser } = req
-      sgMail.setApiKey(config.SENDGRID.SENDGRID_KEY);
-    
-      today = new Date();
-      let day = today.getDay(),
-          diff = today.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
-      const monday = new Date(today.setDate(diff));
-      monday.setHours(0,0,0,0);
-
-      const end = new Date();
-      end.setHours(20,59,59,999);
-      const activity = await Activity.find({user :currentUser.id, created_at: {$gte: monday, $lt: end}}).sort({_id : -1 }).limit(15)
-      let now = moment();
-      const today = now.format("dddd, MMMM Do YYYY")
-      
-      let contacts = []
-      for (let i =0; i < activity.length; i ++){
-        const contact = await Contact.findOne({_id: activity[i].contacts})
-        let content = "<div class='content' style='display:flex; padding-top:20px; margin-right:10px;max-width: 500px;justify-content:space-around;padding-left:30px; border-bottom: 1px solid #afaaaa;'><div class='avatar' style='margin-right:20px;'><img style='margin:auto;' src='" + urls.AVATAR_URL+"' width='60px' height='60px' /></div>" + 
-        "<div class='contact'><h3>" + contact.first_name + "</h3><p style='margin: 0px'>" + contact.email +" " + contact.cell_phone + "</p>" +
-        "<p style='margin: 0px'>" + activity[i].content +"</p></div>" + 
-        "<button style='background-color: white; color:#0078d4; max-width:100px; height:30px; margin: auto 10px; border: 1px solid; border-left: 4px solid #0078d4; cursor:pointer;'><a href='" + urls.CONTACT_PAGE_URL + contact.id + "'>View</a></button></div>"
-        contacts.push(content)
-      }
-    
-      const _follow_up = await FollowUp.find({user :currentUser.id, status: 0, due_date: {$lt: current_time}});
-      let overdue = [];
-      
-      for(let i = 0; i < _follow_up.length; i ++){
-        const contact = await Contact.findOne({_id: _follow_up[i].contact}) 
-        const _overdue = "<div class='content' style='display:flex; padding-top:20px; margin-right:10px;max-width: 500px;justify-content:space-around;padding-left:30px; border-bottom: 1px solid #afaaaa;'><div class='avatar' style='margin-right:20px;'><img style='margin:auto;' src='" + urls.AVATAR_URL+"' width='60px' height='60px' /></div>" + 
-        "<div class='contact'><h3>" + contact.first_name + "</h3><p style='margin: 0px'>" + contact.email +" " + contact.cell_phone + "</p>" +
-        "<p style='margin: 0px'>" + _follow_up[i].content +"</p></div>" + 
-        "<button style='background-color: white; color:#0078d4; max-width:100px; height:30px; margin: auto 10px; border: 1px solid; border-left: 4px solid #0078d4; cursor:pointer;'><a href='" + urls.FOLLOWUP_PAGE_URL + contact.id + "'>View FollowUp</a></button></div>"
-        overdue.push(_overdue)
-      }
-
-      const msg = {
-        to: currentUser.email,
-        from: mail_contents.DAILY_REPORT.MAIL,
-        subject: mail_contents.DAILY_REPORT.SUBJECT,
-        templateId: config.SENDGRID.SENDGRID_DAILY_REPORT_TEMPLATE,
-        dynamic_template_data: {
-          contacts: contacts,
-          overdue: overdue,
-          day: today
-        },
-      }
-      await sgMail.send(msg)
-    },
-    start: true,
-    timeZone: 'US/Central'
-  });
-  
-  console.log('weekly_report', weekly_report)
-  
   return res.send({
     status: true,
-    data: weekly_report
   })
 }
 
@@ -853,10 +851,7 @@ const disconDaily = async(req, res) =>{
   const user = req.currentUser
 
   user['daily_report'] = false;
-
   await user.save()
-
-  daily_report.stop()
   
   return res.send({
     status: true
@@ -867,10 +862,7 @@ const disconWeekly = async(req, res) =>{
   const user = req.currentUser
 
   user['weekly_report'] = false;
-
   await user.save()
-
-  weekly_report.stop()
   
   return res.send({
     status: true
