99ff40cd4d5836cd5c3a0b8971bc909540db5dc4
superwebtop <superwebtop@outlook.com>
2019-06-12 07:34:12 -0700

    video tracker query issue fix

--- controllers/video_tracker.js
+++ controllers/video_tracker.js
@@ -52,7 +52,86 @@ const createbyDesktop = async(req, res) => {
     updated_at: new Date(),
     created_at: new Date(),
   })
-  disconnect(video_tracker.id)
+  const currentUser = await User.findOne({_id: query['user']})
+  const contact = await Contact.findOne({_id: query['contact']})
+  const video = await Video.findOne({_id: query['video']})
+
+  const d = (query['duration']/1000)
+  var h = Math.floor(d / 3600);
+  var m = Math.floor(d % 3600 / 60);
+  var s = Math.floor(d % 3600 % 60);
+
+  if (h   < 10) {h   = "0"+h;}
+  if (m < 10) {m = "0"+m;}
+  if (s < 10) {s = "0"+s;}
+  let timeWatched = h + ':' + m + ':' + s
+
+  const tD = Math.floor(video.duration/1000);
+  var tH = Math.floor(tD / 3600);
+  var tM = Math.floor(tD % 3600 / 60);
+  var tS = Math.floor(tD % 3600 % 60);
+
+  if (tH   < 10) {tH   = "0"+tH;}
+  if (tM < 10) {tM = "0"+tM;}
+  if (tS < 10) {tS = "0"+tS;}
+
+  let timeTotal = tH + ':' + tM + ':' + tS
+
+  // send desktop notification
+  if(currentUser.desktop_notification == true){
+    webpush.setVapidDetails(
+      'mailto:support@teamgrow.co',
+      config.VAPID.PUBLIC_VAPID_KEY,
+      config.VAPID.PRIVATE_VAPID_KEY
+    )
+    
+    const subscription = JSON.parse(currentUser.desktop_notification_subscription)
+    const title = contact.first_name + ' watched video -' + video.title 
+    const body = 'Watched ' + timeWatched + ' of ' + timeTotal + ' at ' + query['time_start']
+    const playload = JSON.stringify({notification: {"title":title, "body":body, "icon": "/fav.ico"}})
+    webpush.sendNotification(subscription, playload).catch(err => console.error(err))
+  }
+
+  // send email notification
+  sgMail.setApiKey(config.SENDGRID.SENDGRID_KEY);
+
+  const msg = {
+    to: currentUser.email,
+    from: mail_contents.NOTIFICATION_SEND_MATERIAL.MAIL,
+    subject: mail_contents.NOTIFICATION_SEND_MATERIAL.SUBJECT,
+    templateId: config.SENDGRID.SENDGRID_NOTICATION_TEMPLATE,
+    dynamic_template_data: {
+      first_name: contact.first_name,
+      last_name: contact.last_name,
+      phone_number: contact.cell_phone,
+      email: contact.email,
+      activity: contact.first_name + ' watched video - <b>' + video.title + '</b>',
+      duration: 'Watched <b>' + timeWatched + ' of ' + timeTotal + ' </b>at ' + query['time_start'],
+      detailed_activity: "<a href='" + urls.CONTACT_PAGE_URL + contact.id + "' style='text-decoration: none;'>View Contact</a>"
+    },
+  };
+
+  await sgMail.send(msg)
+
+  video_tracker.save()
+  .then(_video_tracker => {
+
+    const activity = new Activity({
+      content: contact.first_name + ' watched video',
+      contacts: _video_tracker.contact,
+      user: currentUser.id,
+      type: 'video_trackers',
+      video_trackers: _video_tracker.id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    })
+
+    activity.save().then(_activity => {
+      myJSON = JSON.stringify(_video_tracker)
+      const data = JSON.parse(myJSON);
+      data.activity = _activity
+    })
+  })
 }
 const disconnect = async(video_tracker_id) =>{
   const query = await VideoTracker.findOne({_id: video_tracker_id})
