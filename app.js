8713807d737763742585bd5660fcf9e38a6c01f8
superwebtop <superwebtop@outlook.com>
2019-06-16 11:00:11 -0700

    body parse fix

--- app.js
+++ app.js
@@ -13,8 +13,11 @@ let app = express();
 
 app.use(cors())
 app.use(logger('dev'))
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({ extended: false, limit: '50mb' }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser())
 app.use(cookieParser())
 app.use(express.static('../frontend_admin/dist'));
 app.use('/api', indexRouter)
