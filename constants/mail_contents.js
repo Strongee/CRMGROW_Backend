5dd20790423fc9ce8967cf9012387a54b7c65f87
superwebtop <superwebtop@outlook.com>
2019-08-21 14:24:04 -0700

    test

--- constants/mail_contents.js
+++ constants/mail_contents.js
@@ -1,19 +1,19 @@
 const mail_contents = {
     WELCOME_SIGNUP: {
-        MAIL: "support@crmgrow.com",
+        MAIL: "CRMGROW <no-reply@crmgrow.com>",
         SUBJECT: "Welcome to CRMGROW",
         TEXT: "Welcome to CRMGROW"
     },
     NOTIFICATION_SEND_MATERIAL: {
-        MAIL: "no-reply@crmgrow.com",
+        MAIL: "CRMGROW <no-reply@crmgrow.com>",
         SUBJECT: "CRMGROW video watched notification: "
     },
     DAILY_REPORT: {
-        MAIL: "support@crmgrow.com",
+        MAIL: "CRMGROW <support@crmgrow.com>",
         SUBJECT: "Daily report: "
     },
     RESET_PASSWORD: {
-        MAIL: "support@crmgrow.com",
+        MAIL: "CRMGROW <support@crmgrow.com>",
         SUBJECT: "Rest Password"
     }
 }
