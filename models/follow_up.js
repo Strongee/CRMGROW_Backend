53cfaad2d2e948d66bb106154583c6c084a3c9a6
superwebtop <superwebtop@outlook.com>
2019-09-01 02:50:24 -0700

    follow up reminder

--- models/follow_up.js
+++ models/follow_up.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const FollowUp = mongoose.model('follow_up',{
     user: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
     due_date: Date,
-    content: String,
+    content: { type: String, default: ""},
     contact: [{ type: mongoose.Schema.Types.ObjectId, ref: 'contact' }],
     status: { type: Number, default: 0},
     reminder: Number,
