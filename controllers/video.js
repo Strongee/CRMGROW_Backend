87526424ed7d38eb6f5f371f5f7b4a58dce313bc
superwebtop <superwebtop@outlook.com>
2019-09-27 02:24:07 -0700

    test

--- controllers/video.js
+++ controllers/video.js
@@ -283,10 +283,10 @@ const sendVideo = async (req, res) => {
     subject: subject,
     html: '<html><head><title>Video Invitation</title></head><body><p style="white-space: pre-wrap;">' + content + '</p>'+ 
     +'<a href="' + video_link + '">'
-    +'<img src="'+video_preview+'?resize=true"></img>'
+    +'<img src="'+video_preview+'?resize=true"/>'
     +'</a><br/>'
     + '<a href="' + video_link + '">'
-    +'<img src="'+urls.ASSETS_URL+'images/play-button.png"/></img>' + 
+    +'<img src="'+urls.ASSETS_URL+'images/play-button.png"/>' + 
      '</a><br/><br/>Thank you<br/><br/>'+ currentUser.email_signature + '</body></html>'
   }
 
