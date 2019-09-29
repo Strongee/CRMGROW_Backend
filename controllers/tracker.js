1d08087c299bff1d01ccf271fce83be3276d4e1b
superwebtop <superwebtop@outlook.com>
2019-09-29 00:56:13 -0700

    td styling test

--- controllers/tracker.js
+++ controllers/tracker.js
@@ -231,7 +231,7 @@ const updatePDF = async(duration, pdf_tracker_id) =>{
         email: `<a href="mailto:${contact.email}">${contact.email}</a>`,
         activity: contact.first_name + ' watched video - <b>' + video.title + '</b>',
         duration: 'Watched <b>' + timeWatched + ' of ' + timeTotal + ' </b>at ' + created_at,
-        detailed_activity: "<a href='" + urls.CONTACT_PAGE_URL + contact.id + "' style='text-decoration:none;color:white;font-size:16px;line-height:30px;background-color:#2c6fae;border:none;border-radius:5px;font-size:15px; padding:10px 25px 10px 25px;display:inline-block;'>View Contact</a>"
+        detailed_activity: "<td style='text-decoration:none;color:white;font-size:16px;line-height:23px;background-color:#2c6fae;border:none;border-radius:5px;font-size:15px; padding:10px 25px 10px 25px;display:inline-block;'><a href='" + urls.CONTACT_PAGE_URL + contact.id + "' >View Contact</a></td>"
       },
     };
   
