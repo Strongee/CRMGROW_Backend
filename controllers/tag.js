4abec9e58be4592696ea9321a7229ebd48a46ea6
superwebtop <superwebtop@outlook.com>
2019-09-28 21:35:27 -0700

    tag

--- controllers/tag.js
+++ controllers/tag.js
@@ -82,7 +82,10 @@ const filter = async(req, res) =>{
   // }
 
   const tags = await Tag.find({user: currentUser.id})
+
+    console.log('tag.length', tags.length)
     for(let j=0; j<tags.length-1; j++){
+      console.log('tags.length', tags[j])
       for(let k=j+1; k<tags[j].length; k++){
         console.log('tags[k].content', tags[k].content)
         if(tags[j].content == tags[k].content){
