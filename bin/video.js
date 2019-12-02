1f33a492c857505c00935b16412689a97e23c5af
Super <superwebtop@outlook.com>
2019-12-02 06:44:01 -0800

    url

--- bin/video.js
+++ bin/video.js
@@ -32,13 +32,14 @@ const migrate = async() => {
   if(videos){
     for(let i=0; i<videos.length; i++){
       const video = videos[i]
-      
+  
       if(!video['preview'] && new Date(video['created_at']).getMonth()>=9){
         let url =  video.url
         const params = {
           Bucket: 'teamgrow',
           Key: url.slice(44)
         }
+        console.log('video', url)
         const file_path = TEMP_PATH + uuidv1()
         let fileStream = fs.createWriteStream(file_path);
         let s3Stream = s3.getObject(params).createReadStream();
