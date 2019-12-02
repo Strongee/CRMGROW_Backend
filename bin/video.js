971110ede672f77b9408bcc9caf7d21218727756
Super <superwebtop@outlook.com>
2019-12-02 07:07:04 -0800

    video

--- bin/video.js
+++ bin/video.js
@@ -32,7 +32,7 @@ const migrate = async() => {
   if(videos){
     for(let i=0; i<videos.length; i++){
       const video = videos[i]
-      if(!video['preview'] && new Date(video['created_at'].getMonth()>=9 && video['url'].indexOf('amazonaws.com')!== -1)){
+      if(!video['preview'] && new Date(video['created_at'].getMonth()>=9 && video['url'].indexOf('amazonaws.com')> 1)){
         let url =  video.url
         const params = {
           Bucket: 'teamgrow',
