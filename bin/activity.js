a2bd61ec733cb0dacbd5f073ce76053e3c1ee0a7
Super <superwebtop@outlook.com>
2019-12-01 17:46:50 -0800

    activity migrate

--- bin/activity.js
+++ bin/activity.js
@@ -20,19 +20,22 @@ const migrate = async() => {
             console.log('err', err)
         })
         if(contacts){
+            console.log('contact length**************', contacts.length)
             for(let j=0; j<contacts.length; j++){
                 const contact = contacts[j]
                 console.log(contact.id)
-                const last_activity = await Activity.find({contacts: contact.id}).sort({'updated_at': -1}).limit(1).catch(err=>{
-                    console.log('err', err)
-                })
-                if(last_activity[0]){
-                    console.log('last_activity.id',last_activity[0].id)
-                    contact['last_activity'] = last_activity[0].id
-                    contact.save().catch(err=>{
+                if(!contacts[j].last_activity){
+                    const last_activity = await Activity.find({contacts: contact.id}).sort({'updated_at': -1}).limit(1).catch(err=>{
                         console.log('err', err)
                     })
-                }
+                    if(last_activity[0]){
+                        console.log('last_activity.id',last_activity[0].id)
+                        contact['last_activity'] = last_activity[0].id
+                        contact.save().catch(err=>{
+                            console.log('err', err)
+                        })
+                    }
+                }  
             }
         }
     }
