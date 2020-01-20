cc04c9e6203320a98e2435594feb6f91c843e3f6
silverbeauty <superwebtop@outlook.com>
2020-01-20 09:48:25 -0800

    video

--- controllers/video.js
+++ controllers/video.js
@@ -808,6 +808,7 @@ const bulkGmail = async(req, res) => {
 
 
   const token = JSON.parse(currentUser.google_refresh_token)
+  console.log('token****', token)
   const smtpTransport = nodemailer.createTransport({
     service: "gmail",
     auth: {
