b79e919a9f0942b32fd9455696385da2e9b4bedf
silverbeauty <superwebtop@outlook.com>
2020-01-26 13:22:35 -0800

    user admin login

--- bin/user.js
+++ bin/user.js
@@ -23,3 +23,25 @@
 //   }
 // }
 // migrate();
+const mongoose = require('mongoose');
+const User = require('../models/user')
+const { DB_PORT } = require('../config/database');
+
+mongoose.set('useCreateIndex', true)
+mongoose.connect(DB_PORT, {useNewUrlParser: true})
+.then(() => console.log('Connecting to database successful'))
+.catch(err => console.error('Could not connect to mongo DB', err))
+//Fetch or read data from
+const migrate = async() => {
+  const users = await User.find({}).catch(err=>{
+    console.log('err', err)
+  })
+  for(let i=0; i<users.length; i++){
+    const user = users[i]
+    user['admin_loggin'] = false
+    user.save().catch(err=>{
+      console.log('err', err)
+    })
+  }
+}
+migrate();
