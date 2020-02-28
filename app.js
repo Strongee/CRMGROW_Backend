d0f5534633064672c923e336823e2aba8a764f80
silverbeauty <superwebtop@outlook.com>
2020-02-27 21:44:38 -0800

    frontend

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false, limit: '50mb' }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.use(express.static('../frontend_admin/dist'));
+app.use(express.static('../frontend/dist'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/video', VideoCtrl.play)
