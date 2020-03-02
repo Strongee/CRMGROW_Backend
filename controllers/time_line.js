46080232b81585defa1194a57836f6a561a1c643
LOMFM <brightdawn019@gmail.com>
2020-03-03 03:05:08 +0800

    timeline reassign

--- controllers/time_line.js
+++ controllers/time_line.js
@@ -12,7 +12,92 @@ const moment = require('moment')
 const create = async(req, res) => {
   const { currentUser } = req
   const { contact, automation_id } = req.body
+ 
+  const _automation = await Automation.findOne({_id: automation_id}).catch(err=>{
+    console.log('err', err)
+    res.status(400).json({
+      status: false,
+      err: err.message || 'Automation found err'
+    })
+  })
   
+  if(_automation){
+    const automations = _automation['automations']
+    for(let i=0; i<automations.length; i++){
+      const automation = automations[i]
+      let time_line
+      if(automation['status'] == 'active'){
+        const period = automation['period']
+        let now = moment()
+        let due_date = now.add(period, 'hours');
+        due_date.set({minute:0,second:0,millisecond:0})
+        _time_line = new TimeLine({
+          ...automation,
+          ref: automation.id,
+          parent_ref: automation.parent,
+          user: currentUser.id,
+          contact: contact,
+          automation: automation_id,
+          due_date: due_date,
+          created_at: new Date(),
+          updated_at: new Date()
+        })
+        _time_line.save().then(timeline =>{
+          if(timeline['period'] == 0){
+            try{
+              runTimeline(timeline.id)
+              const data = {
+                contact: contact,
+                ref: timeline.ref,
+              }
+              activeNext(data) 
+            }catch(err){
+              console.log('err', err)
+            }
+          }
+        }).catch(err=>{
+          console.log('err', err)
+        })
+      } else {
+        time_line = new TimeLine({
+          ...automation,
+          ref: automation.id,
+          parent_ref: automation.parent,
+          user: currentUser.id,
+          contact: contact,
+          automation: automation_id,
+          created_at: new Date(),
+          updated_at: new Date()
+        })
+        time_line.save().catch(err=>{
+          console.log('err', err)
+        })
+      }
+    }
+    return res.send({
+      status: true
+    })
+  } else {
+    res.status(400).json({
+      status: false,
+      err: 'Automation not found'
+    })
+  }
+}
+
+
+const reCreate = async(req, res) => {
+  const { currentUser } = req
+  const { contact, automation_id } = req.body
+
+  await TimeLine.deleteMany({contact: contact}).catch(err => {
+    console.log(err);
+    return res.json({
+      status: false,
+      error: err.message + ' in removing current timelines.' || 'Current Timeline Removing Error.'
+    })
+  })
+    
   const _automation = await Automation.findOne({_id: automation_id}).catch(err=>{
     console.log('err', err)
     res.status(400).json({
@@ -432,6 +517,7 @@ const cancel = (req, res) => {
 
 module.exports = {
   create,
+  reCreate,
   activeNext,
   runTimeline,
   cancel
