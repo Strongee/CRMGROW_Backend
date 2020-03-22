778f45ad0e9d161599852dbfa4dbf227bf2a7714
silverbeauty <superwebtop@outlook.com>
2020-03-22 11:22:39 -0700

    path config

--- config/path.js
+++ config/path.js
@@ -1,3 +1,7 @@
+/** 
+ ** 
+  For Linux
+*/
 module.exports.FILES_PATH = '/var/www/teamgrow/files/'
 module.exports.THUMBNAILS_PATH = '/var/www/teamgrow/thumbnails/'
 module.exports.GIF_PATH = '/var/www/teamgrow/gif/'
@@ -6,3 +10,15 @@ module.exports.TEMP_PATH = '/var/www/teamgrow/temp/'
 module.exports.VIDEO_PATH = '/var/www/teamgrow/video/'
 module.exports.ENV_PATH = '/var/www/teamgrow/teamgrow_env'
 module.exports.TRAKER_PATH = '/var/www/teamgrow/backend_admin/tracker.jpg'
+
+/**
+  For Windows, Azure
+module.exports.FILES_PATH = '../files/'
+module.exports.THUMBNAILS_PATH = '../thumbnails/'
+module.exports.GIF_PATH = '../gif/'
+module.exports.PREVIEW_PATH = '../previews/'
+module.exports.TEMP_PATH = '../temp/'
+module.exports.VIDEO_PATH = '../video/'
+module.exports.ENV_PATH = '../teamgrow_env'
+module.exports.TRAKER_PATH = './tracker.jpg'
+*/
