0b75841a0121a76fd454ebdedde6b7dc078fc8c5
silverbeauty <superwebtop@outlook.com>
2020-03-28 22:25:20 -0700

    user

--- bin/admin.js
+++ bin/admin.js
@@ -13,67 +13,75 @@ const User = require('../models/user')
 const Activity = require('../models/activity')
 const addContacts = async() => {
   
-  const admin = await User.findOne({email: 'support@crmgrow.com'}).catch('err', err)
-  const users = await User.find({del: false}).catch('err', err)
-  for(let i=0; i<users.length; i++){
-    const user = users[i]
-    let contact
-    if(user.payment) {
-      if(user.subscription && user.subscription.is_suspended) {
-        contact = new Contact({
-          first_name: user.user_name.split(" ")[0],
-          last_name: user_name.split(" ")[1],
-          email: user.email,
-          cell_phone: user.cell_phone,
-          tags: ['suspended'],
-          created_at: user.created_at,
-          user: admin.id
-        })
+  const admin = await User.findOne({email: 'support@crmgrow.com'}).catch(err => {
+    console.log('err', err)
+  })
+  if(admin){
+    console.log('admin', admin)
+    const users = await User.find({del: false}).catch(err => {
+      console.log('err', err)
+    })
+    for(let i=0; i<users.length; i++){
+      const user = users[i]
+      let contact
+      if(user.payment) {
+        if(user.subscription && user.subscription.is_suspended) {
+          contact = new Contact({
+            first_name: user.user_name.split(" ")[0],
+            last_name: user_name.split(" ")[1],
+            email: user.email,
+            cell_phone: user.cell_phone,
+            tags: ['suspended'],
+            created_at: user.created_at,
+            user: admin.id
+          })
+        } else {
+          contact = new Contact({
+            first_name: user.user_name.split(" ")[0],
+            last_name: user_name.split(" ")[1],
+            email: user.email,
+            cell_phone: user.cell_phone,
+            tags: ['active'],
+            created_at: user.created_at,
+            user: admin.id
+          })
+        }
       } else {
         contact = new Contact({
           first_name: user.user_name.split(" ")[0],
           last_name: user_name.split(" ")[1],
           email: user.email,
           cell_phone: user.cell_phone,
-          tags: ['active'],
           created_at: user.created_at,
+          tags: ['free'],
           user: admin.id
         })
+        
       }
-    } else {
-      contact = new Contact({
-        first_name: user.user_name.split(" ")[0],
-        last_name: user_name.split(" ")[1],
-        email: user.email,
-        cell_phone: user.cell_phone,
-        created_at: user.created_at,
-        tags: ['free'],
-        user: admin.id
-      })
       
-    }
-    
-    contact.save().then(_contact=>{
-      const activity = new Activity({
-        content: 'added contact',
-        contacts: _contact.id,
-        user: admin.id,
-        type: 'contacts',
-        created_at: new Date(),
-        updated_at: new Date(),
-      })
-  
-      activity.save().then(_activity => {
-        _contact['last_activity'] = _activity.id
-        _contact.save().then(__contact=>{
-          console.log('email', __contact.email)
-        }).catch(err => {
-          console.log('err', err)
+      contact.save().then(_contact=>{
+        const activity = new Activity({
+          content: 'added contact',
+          contacts: _contact.id,
+          user: admin.id,
+          type: 'contacts',
+          created_at: new Date(),
+          updated_at: new Date(),
         })
+    
+        activity.save().then(_activity => {
+          _contact['last_activity'] = _activity.id
+          _contact.save().then(__contact=>{
+            console.log('email', __contact.email)
+          }).catch(err => {
+            console.log('err', err)
+          })
+        })
+      }).catch(err=>{
+        console.log('err', err)
       })
-    }).catch(err=>{
-      console.log('err', err)
-    })
+    }
   }
+  
 }
 addContacts()
