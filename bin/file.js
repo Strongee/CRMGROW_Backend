1fc4833f5a5f0ae574120143febc1f5c3551aeda
silverbeauty <superwebtop@outlook.com>
2020-04-20 00:33:28 -0700

    picture profile

--- bin/file.js
+++ bin/file.js
@@ -21,7 +21,7 @@ mongoose.connect(DB_PORT, {useNewUrlParser: true})
 
 
 const url_job = async() => {
-  const videos = await Video.find({del: false, converted: false, type: {$nin: ['application/octet-stream', 'youtube', 'vimeo']}}).catch(err=>{
+  const video = await Video.findOne({_id: mongoose.Types.ObjectId("5e5982106ec2d73427e6dbf7")}).catch(err=>{
     console.log('videos err', err.message)
   })
   for(let i = 0; i <videos.length; i++){
@@ -88,5 +88,71 @@ const url_job = async() => {
   }
 }
 
-url_job()
+function readFileStream(fileName, cb, options) {
+  return new Promise(function(resolve, reject) {
+      var readable = fs.createReadStream(fileName, options);
 
+      readable.on('data', function(data) {
+          cb(data);
+      })
+
+      readable.on('end', function() {
+          resolve();
+      })
+
+      readable.on('error', function(err) {
+          return reject(err);
+      })
+  })
+}
+
+function printDataAsItComesIn(data) {
+  // Data will be null when it reaches the end.
+  if (!data) return;
+  
+  // type of data = Buffer
+  var stringData = data.toString('utf8', 0, data.length);
+  console.log(stringData.length);
+}
+
+const large_job = async() => {
+  const video = await Video.findOne({_id: mongoose.Types.ObjectId("5e5982106ec2d73427e6dbf7")}).catch(err=>{
+    console.log('videos err', err.message)
+  })
+  if (video['url']) {
+    const file_name = video['url'].slice(39)
+    console.log('path', file_name)
+    if (fs.existsSync(TEMP_PATH+file_name)) {  
+      fs.readFile(TEMP_PATH+file_name, (err, data) => {
+        if (err){
+          console.log('file read err', err)
+        }else {
+          console.log('File read was successful', data)
+          const today = new Date()
+          const year = today.getYear()
+          const month = today.getMonth()
+          const params = {
+              Bucket: config.AWS.AWS_S3_BUCKET_NAME, // pass your bucket name
+              Key: 'video' +  year + '/' + month + '/' + file_name, 
+              Body: data,
+              ACL: 'public-read'
+          };
+          s3.upload(params, async (s3Err, upload)=>{
+            if (s3Err){
+              console.log('upload s3 error', s3Err)
+            } else {
+              console.log(`File uploaded successfully at ${upload.Location}`)
+            
+              video['url'] = upload.Location
+              video['converted'] = true
+              video.save().catch(err=>{
+                console.log('video save error', err.message)
+              })
+            }
+          })
+        }
+     });
+    }
+  }
+}
+large_job()
