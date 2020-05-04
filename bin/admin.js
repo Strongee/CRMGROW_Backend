b6ba10577e4205d08e1d8c02d64ffa8bcc3e5903
silverbeauty <superwebtop@outlook.com>
2020-05-04 15:23:23 -0700

    migration

--- bin/admin.js
+++ bin/admin.js
@@ -24,10 +24,31 @@ const addContacts = async() => {
     for(let i=0; i<users.length; i++){
       const user = users[i]
       let contact
+      let label
       const old_user = await Contact.findOne({email: user.email, user: admin.id}).catch(err=>{
         console.log('err', err)
       })
       if(!old_user){
+        let week_ago = new Date()
+        let month_ago = new Date()
+        let two_month_ago = new Date()
+        week_ago.setDate(week_ago.getDate()-7)
+        month_ago.setMonth(month_ago.getMonth() - 1);
+        two_month_ago.setMonth(two_month_ago.getMonth() - 1);
+        if(user.last_logged) {
+          let last_logged = new Date(user.last_logged)
+          let created = new Date(user.created_at)
+          
+          if(created.getTime()>week_ago.getTime()) {
+            label = 'New'
+          }else if(last_logged.getTime()>week_ago.getTime()){
+            label = 'Hot'
+          } else if(last_logged.getTime()>month_ago.getTime()){
+            label = 'Warm'
+          } else {
+            label = 'Cold'
+          }
+        }
         if(user.payment) {
           if(user.subscription && user.subscription.is_suspended) {
             contact = new Contact({
@@ -35,7 +56,19 @@ const addContacts = async() => {
               last_name: user.user_name.split(" ")[1],
               email: user.email,
               cell_phone: user.cell_phone,
-              tags: ['suspended'],
+              tags: ['suspended', user.company],
+              label: label,
+              created_at: user.created_at,
+              user: admin.id
+            })
+          } else if(user.subscription && user.subscription.is_failed) {
+            contact = new Contact({
+              first_name: user.user_name.split(" ")[0],
+              last_name: user.user_name.split(" ")[1],
+              email: user.email,
+              cell_phone: user.cell_phone,
+              tags: ['failed', user.company],
+              label: label,
               created_at: user.created_at,
               user: admin.id
             })
@@ -45,19 +78,21 @@ const addContacts = async() => {
               last_name: user.user_name.split(" ")[1],
               email: user.email,
               cell_phone: user.cell_phone,
-              tags: ['active'],
+              tags: ['active',  user.company],
+              label: label,
               created_at: user.created_at,
               user: admin.id
             })
           }
-        } else {
+        }  if(user.last_logged && new Date(user.last_logged.getTime())>two_month_ago.getTime()){
           contact = new Contact({
             first_name: user.user_name.split(" ")[0],
             last_name: user.user_name.split(" ")[1],
             email: user.email,
             cell_phone: user.cell_phone,
             created_at: user.created_at,
-            tags: ['free'],
+            tags: ['free', user.company],
+            label: label,
             user: admin.id
           })
           
