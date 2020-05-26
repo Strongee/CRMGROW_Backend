db329c6cba8b8c70a0dbcdb9049625d3692d00da
silverbeauty <superwebtop@outlook.com>
2020-05-26 11:26:48 -0700

    tracker path

--- config/path.js
+++ config/path.js
@@ -7,4 +7,4 @@ module.exports.PREVIEW_PATH = path.join(__dirname, '../..', 'previews');
 module.exports.TEMP_PATH = path.join(__dirname, '../..', 'temp');
 module.exports.VIDEO_PATH = path.join(__dirname, '../..', 'video');
 module.exports.ENV_PATH = path.join(__dirname, '..', '.env');
-module.exports.TRAKER_PATH = path.join(__dirname, '../..', 'tracker.jpg');
+module.exports.TRAKER_PATH = path.join(__dirname, '..', 'tracker.jpg');
