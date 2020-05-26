f8ffe5a3eb3fa776a75da53ba04616185a1dfe09
silverbeauty <superwebtop@outlook.com>
2020-05-26 00:24:46 -0700

    cron

--- config/path.js
+++ config/path.js
@@ -1,31 +1,3 @@
-/*
-  For Linux
-*/
-// module.exports.FILES_PATH = '/var/www/teamgrow/files/';
-// module.exports.THUMBNAILS_PATH = '/var/www/teamgrow/thumbnails/';
-// module.exports.GIF_PATH = '/var/www/teamgrow/gif/';
-// module.exports.PREVIEW_PATH = '/var/www/teamgrow/previews/';
-// module.exports.TEMP_PATH = '/var/www/teamgrow/temp/';
-// module.exports.VIDEO_PATH = '/var/www/teamgrow/video/';
-// module.exports.VIDEO_CONVERT_LOG_PATH = '/var/www/teamgrow/video_log/';
-// module.exports.ENV_PATH = '../.env';
-// module.exports.TRAKER_PATH = '/var/www/teamgrow/backend_admin/tracker.jpg';
-// module.exports.PLAY_BUTTON_PATH =
-//   '/var/www/teamgrow/backend_admin/play-button.png';
-
-/*
-  For Windows, Azure
-*/
-// module.exports.FILES_PATH = '../files/';
-// module.exports.THUMBNAILS_PATH = '../thumbnails/';
-// module.exports.GIF_PATH = '../gif/';
-// module.exports.PREVIEW_PATH = '../previews/';
-// module.exports.TEMP_PATH = '../temp/';
-// module.exports.VIDEO_PATH = '../video/';
-// module.exports.ENV_PATH = '../teamgrow_env';
-// module.exports.TRAKER_PATH = './tracker.jpg';
-
-/* For Mac */
 const path = require('path');
 
 module.exports.FILES_PATH = path.join(__dirname, '..', 'files');
