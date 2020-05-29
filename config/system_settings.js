323bb1f2918226e531362148f1cdaf2f715a78ae
silverbeauty <superwebtop@outlook.com>
2020-05-29 01:53:56 -0700

    config remove on gitignore

---
+++ config/system_settings.js
@@ -0,0 +1,28 @@
+const system_settings = {
+  SUBSCRIPTION_MONTHLY_PLAN: {
+    BASIC: '29',
+    TEAM: '25',
+  },
+  EMAIL_DAILY_LIMIT: {
+    BASIC: 300,
+  },
+  TEXT_MONTHLY_LIMIT: {
+    BASIC: 250,
+  },
+  CONTACT_UPLOAD_LIMIT: {
+    BASIC: 3000,
+  },
+  MATERIAL_UPLOAD_LIMIT: {
+    BASIC: 15,
+  },
+  PASSWORD: {
+    USER: process.env.DEFAULT_PASS || 'admin123',
+    ADMIN: process.env.ADMIN_DEFAULT_PASS || 'admin123',
+  },
+  COMPANY: {
+    DEFAULT: 'eXp Realty',
+  },
+  ADMIN_ACCOUNT: 'support@crmgrow.com',
+};
+
+module.exports = system_settings;
