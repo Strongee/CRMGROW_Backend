0ae5226e98b4830430e71b848d0651f829d902b7
joe <joe@2e18.com>
2020-06-08 20:27:13 -0700

    ECS Files

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
+let express = require('express');
 const path = require('path');
-const fs = require('fs');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { ENV_PATH, FILES_PATH } = require('./config/path');
+const { ENV_PATH } = require('./config/path');
 require('dotenv').config();
 
-const indexRouter = require('./routes/index.js');
+let indexRouter = require('./routes/index.js');
 const VideoCtrl = require('./controllers/video');
 const PDFCtrl = require('./controllers/pdf');
 const ImageCtrl = require('./controllers/image');
 const PageCtrl = require('./controllers/page');
 const EmailCtrl = require('./controllers/email');
 const { catchError } = require('./controllers/error');
+let app = express();
 
-const app = express();
 app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -26,8 +25,7 @@ app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static('../frontend/dist'));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/video', catchError(VideoCtrl.play));
 app.get('/video1/:id', catchError(VideoCtrl.play1));
@@ -37,15 +35,12 @@ app.get('/image', catchError(ImageCtrl.play));
 app.get('/image/:id', catchError(ImageCtrl.play1));
 app.get('/embed/video/:video', catchError(VideoCtrl.embedPlay));
 app.get('/unsubscribe/:id', catchError(EmailCtrl.unSubscribeEmail));
-app.get('/resubscribe/:id', catchError(EmailCtrl.reSubscribeEmail));
 
 app.get('/auth', (req, res) => {
   res.render('auth');
 });
 
 app.use('/api', indexRouter);
-app.get('*', catchError(PageCtrl.display), (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-});
+app.get('*', catchError(PageCtrl.display));
 
 module.exports = app;
