ee4ce92cd0589ecde2423a9d99d24d3b578ae394
silverbeauty <superwebtop@outlook.com>
2020-07-28 21:21:41 -0500

    admin cron job fix

--- models/contact.js
+++ models/contact.js
@@ -63,48 +63,6 @@ const LABEL = [
   'Appt Missed',
   'Lead',
 ];
-const Labels = [
-  {
-    id: undefined,
-    name: '',
-  },
-  {
-    id: '5f16d58d0af09220208b6e09',
-    name: 'New',
-  },
-  {
-    id: '5f16d58d0af09220208b6e0a',
-    name: 'Cold',
-  },
-  {
-    id: '5f16d58d0af09220208b6e0b',
-    name: 'Team',
-  },
-  {
-    id: '5f16d58d0af09220208b6e0c',
-    name: 'Warm',
-  },
-  {
-    id: '5f16d58d0af09220208b6e0d',
-    name: 'Hot',
-  },
-  {
-    id: '5f16d58d0af09220208b6e0e',
-    name: 'Trash',
-  },
-  {
-    id: '5f16d58d0af09220208b6e0f',
-    name: 'Appt Set',
-  },
-  {
-    id: '5f16d58d0af09220208b6e10',
-    name: 'Appt Missed',
-  },
-  {
-    id: '5f16d58d0af09220208b6e11',
-    name: 'Lead',
-  },
-];
 const States = [
   '',
   'Alabama',
