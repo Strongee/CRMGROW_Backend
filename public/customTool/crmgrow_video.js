50322d85d2842efc62e0d864decd2a4630cafb2b
LOMFM <brightdawn019@gmail.com>
2020-10-21 06:08:03 +0100

    Unlayer Custom Tool
    Test the Unlayer Custom Tool Option

--- public/customTool/crmgrow_video.js
+++ public/customTool/crmgrow_video.js
@@ -1,22 +1,22 @@
 unlayer.registerTool({
     name: 'my_tool',
     label: 'My Tool',
-    icon: 'fa-smile',
+    icon: 'fa-photo-video',
     supportedDisplayModes: ['web', 'email'],
     options: {},
     values: {},
     renderer: {
       Viewer: unlayer.createViewer({
         render(values) {
-          return "<div>I am a custom tool.</div>"
+          return "<div>I am a custom tool Editor UI.</div>"
         }
       }),
       exporters: {
         web: function(values) {
-          return "<div>I am a custom tool.</div>"
+          return "<div>I am a custom tool Exporter WEB.</div>"
         },
         email: function(values) {
-          return "<div>I am a custom tool.</div>"
+          return "<div>I am a custom tool Email Exporter.</div>"
         }
       },
       head: {
