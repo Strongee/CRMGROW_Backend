d3f00b3e3537f6a75fb29516322780ed3249da82
silverbeauty <30655770+silverbeauty@users.noreply.github.com>
2021-02-11 11:56:45 -0500

    front and staging

--- constants/urls.js
+++ constants/urls.js
@@ -1,9 +1,9 @@
 let domain = 'https://app.crmgrow.com/';
-let front = 'teamgrow-staticsite-green.s3-website-us-west-1.amazonaws.com/';
+const front = 'teamgrow-staticsite-green.s3-website-us-west-1.amazonaws.com/';
 if (process.env.NODE_ENV === 'production') {
   domain = 'https://app.crmgrow.com';
 } else if (process.env.NODE_ENV === 'staging') {
-  domain = 'https://ecsbe.crmgrow.com';
+  domain = 'http://3.139.190.87:3000';
 } else {
   domain = 'http://localhost:3000';
 }
