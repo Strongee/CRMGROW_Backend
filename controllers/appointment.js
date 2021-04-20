7f5dd13e8992fb3e0c301b1d793fbd814a2ade37
LOMFM <brightdawn019@gmail.com>
2021-04-20 16:10:43 -0500

    get event api update

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -2044,15 +2044,15 @@ const getGoogleEventById = async (data) => {
     eventId: remove_id,
   };
   return new Promise((resolve, reject) => {
-    calendar.events.get(params, {}, function (err, event) {
+    calendar.events.get(params, {}, function (err, response) {
       if (err) {
         console.log(
           `There was an error contacting the Calendar service: ${err}`
         );
         reject(err);
       } else {
-        console.log(event);
-        resolve(event);
+        console.log(response.data);
+        resolve(response.data);
       }
     });
   });
